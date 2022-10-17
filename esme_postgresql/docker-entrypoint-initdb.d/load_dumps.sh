21e2351be918fe6c8fcf1ba7d70f45c130aec948
Luc Marchand <luc.marchand@octo.com>
2022-10-17 19:42:28 +0200

    update docker installation and add idea bash suggests

--- esme_postgresql/docker-entrypoint-initdb.d/load_dumps.sh
+++ esme_postgresql/docker-entrypoint-initdb.d/load_dumps.sh
@@ -3,11 +3,11 @@ set -e
 
 databases=("bio_icare_drone" "bloon_project" "pizza_shop" "sportsdb" "transtats")
 
-for database in ${databases[@]}; do
+for database in "${databases[@]}"; do
   psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
       CREATE DATABASE ${database};
       GRANT ALL PRIVILEGES ON DATABASE ${database} TO postgres;
 EOSQL
 
-  psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "${database}" < /docker-entrypoint-initdb.d/${database}.sql.dump
+  psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "${database}" < /docker-entrypoint-initdb.d/"${database}".sql.dump
 done
